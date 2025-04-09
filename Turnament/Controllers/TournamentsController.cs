8f16967bdc6b0140cffe11f154466d39ce04ffe5
Seweryn Mik <133761279+sewerynmik@users.noreply.github.com>
2025-04-09 22:29:30 +0200

    Basic Controllers

---
+++ Turnament/Controllers/TournamentsController.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Turnament.Data;
+using Turnament.Models;
+
+namespace Turnament.Controllers
+{
+    public class TournamentsController(AppDbContext context) : Controller
+    {
+        // GET: Tournaments
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = context.Tournaments.Include(t => t.BracketType).Include(t => t.Creator).Include(t => t.Sport).Include(t => t.WinnerTeam);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Tournaments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await context.Tournaments
+                .Include(t => t.BracketType)
+                .Include(t => t.Creator)
+                .Include(t => t.Sport)
+                .Include(t => t.WinnerTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Create
+        public IActionResult Create()
+        {
+            ViewData["BracketTypeId"] = new SelectList(context.BracketTypes, "Id", "Id");
+            ViewData["CreatorId"] = new SelectList(context.Users, "Id", "Id");
+            ViewData["SportId"] = new SelectList(context.Sports, "Id", "Id");
+            ViewData["WinnerTeamId"] = new SelectList(context.Teams, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tournaments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,SportId,BracketTypeId,CreatorId,StartDate,EndDate,WinnerTeamId")] Tournament tournament)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(tournament);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BracketTypeId"] = new SelectList(context.BracketTypes, "Id", "Id", tournament.BracketTypeId);
+            ViewData["CreatorId"] = new SelectList(context.Users, "Id", "Id", tournament.CreatorId);
+            ViewData["SportId"] = new SelectList(context.Sports, "Id", "Id", tournament.SportId);
+            ViewData["WinnerTeamId"] = new SelectList(context.Teams, "Id", "Id", tournament.WinnerTeamId);
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await context.Tournaments.FindAsync(id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+            ViewData["BracketTypeId"] = new SelectList(context.BracketTypes, "Id", "Id", tournament.BracketTypeId);
+            ViewData["CreatorId"] = new SelectList(context.Users, "Id", "Id", tournament.CreatorId);
+            ViewData["SportId"] = new SelectList(context.Sports, "Id", "Id", tournament.SportId);
+            ViewData["WinnerTeamId"] = new SelectList(context.Teams, "Id", "Id", tournament.WinnerTeamId);
+            return View(tournament);
+        }
+
+        // POST: Tournaments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,SportId,BracketTypeId,CreatorId,StartDate,EndDate,WinnerTeamId")] Tournament tournament)
+        {
+            if (id != tournament.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(tournament);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TournamentExists(tournament.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BracketTypeId"] = new SelectList(context.BracketTypes, "Id", "Id", tournament.BracketTypeId);
+            ViewData["CreatorId"] = new SelectList(context.Users, "Id", "Id", tournament.CreatorId);
+            ViewData["SportId"] = new SelectList(context.Sports, "Id", "Id", tournament.SportId);
+            ViewData["WinnerTeamId"] = new SelectList(context.Teams, "Id", "Id", tournament.WinnerTeamId);
+            return View(tournament);
+        }
+
+        // GET: Tournaments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tournament = await context.Tournaments
+                .Include(t => t.BracketType)
+                .Include(t => t.Creator)
+                .Include(t => t.Sport)
+                .Include(t => t.WinnerTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tournament == null)
+            {
+                return NotFound();
+            }
+
+            return View(tournament);
+        }
+
+        // POST: Tournaments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tournament = await context.Tournaments.FindAsync(id);
+            if (tournament != null)
+            {
+                context.Tournaments.Remove(tournament);
+            }
+
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TournamentExists(int id)
+        {
+            return context.Tournaments.Any(e => e.Id == id);
+        }
+    }
+}

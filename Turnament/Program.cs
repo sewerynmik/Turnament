ba3df3644211184340f88fae3ce4dad9cd12c447
Seweryn Mik <133761279+sewerynmik@users.noreply.github.com>
2025-04-09 21:31:30 +0200

    Seed

--- Turnament/Program.cs
+++ Turnament/Program.cs
@@ -31,5 +31,12 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+/*using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    DbInitializer.Initialize(context);
+}*/
+
 
 app.Run();
